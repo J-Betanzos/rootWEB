ddb1bb4759bcb909127ab0d084b16c11c11e55d2
JBetanzos <julio_betanzos@hotmail.es>
2018-06-18 23:40:46 +0200

    personalized header img

--- DB/script.js
+++ DB/script.js
@@ -1,7 +1,39 @@
- 
-    var type = document.getElementById("type").childNodes 
-    var bg = document.getElementsByTagName("header")
+$('document').ready(() => { 
+
+    const $type = $('#type');
+    const $book = $('#book_type');
+    const $comic = $('#comic_type');
+    const $film = $('#film_type');
+    const $game = $('#game_type');
+    const $bg = $('#head');
+   
+    $book.on('click', event => { 
+        $bg.addClass('book_bg');
+        $bg.removeClass('comic_bg');
+        $bg.removeClass('film_bg');
+        $bg.removeClass('game_bg');
+    });
+
+    $comic.on('click', event => { 
+        $bg.removeClass('book_bg');
+        $bg.addClass('comic_bg');
+        $bg.removeClass('film_bg');
+        $bg.removeClass('game_bg');
+    });
+
+    $film.on('click', event => { 
+        $bg.removeClass('book_bg');
+        $bg.removeClass('comic_bg');
+        $bg.addClass('film_bg');
+        $bg.removeClass('game_bg');
+    });
+
+    $game.on('click', event => { 
+        $bg.removeClass('book_bg');
+        $bg.removeClass('comic_bg');
+        $bg.removeClass('film_bg');
+        $bg.addClass('game_bg');
+    });
     
-    if (type[0]) {
-        bg.backgroundImage.url = "book_bg.jpg"
-    }
+
+});
