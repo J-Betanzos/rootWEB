29286bce6501d9fdcae26bbc583ce5c3d264a538
JBetanzos <julio_betanzos@hotmail.es>
2018-06-18 22:17:32 +0200

    input js

---
+++ DB/script.js
@@ -0,0 +1,7 @@
+ 
+    var type = document.getElementById("type").childNodes 
+    var bg = document.getElementsByTagName("header")
+    
+    if (type[0]) {
+        bg.backgroundImage.url = "book_bg.jpg"
+    }
